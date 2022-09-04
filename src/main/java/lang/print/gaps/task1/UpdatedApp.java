492d253d595d277119f2b7a371aa6f8ab561fddf
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-04 14:22:13 +0300

    Update UpdatedApp.java

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,8 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.println("I'm updated");
+        System.out.println("\n");
+        System.out.println("can be compiled. And can write to console");
     }
 }
