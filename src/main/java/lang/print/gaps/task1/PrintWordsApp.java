a70793efbf9584a40cb9122bcc3edc083d989327
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-04 14:34:13 +0300

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        logger.log
         System.out.println("java");
         System.out.println("is");
         System.out.println("smth");
